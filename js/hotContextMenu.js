080f8da10b530ab9193565c29a1bc8f1e8533c1f
Jean-Marie Ravel <33331141+JiMouse@users.noreply.github.com>
2022-07-08 14:52:44 +0200

    Update hotContextMenu.js

--- js/hotContextMenu.js
+++ js/hotContextMenu.js
@@ -2,19 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
   hot.updateSettings({
     contextMenu: {
-      callback: function(key, options) {
-        if (key === 'about') {
-          setTimeout(function() {
-            alert("This is a context menu with default and custom options mixed");
-          }, 100);
-        }
-      },
+      // callback: function(key, options) {
+      //   if (key === 'about') {
+      //     setTimeout(function() {
+      //       alert("This is a context menu with default and custom options mixed");
+      //     }, 100);
+      //   }
+      // },
       items: {
         // "about": {
         //   name: 'About this menu'
         // },
-        "row_above": {},
-        "row_below": {},
+        // "row_above": {},
+        // "row_below": {},
         "remove_row": {},
         "copy": {},
         "cut": {},
