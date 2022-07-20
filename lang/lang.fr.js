e7dd43254d68276c899f186135c7834679e2979c
Jean-Marie Ravel <33331141+JiMouse@users.noreply.github.com>
2022-07-20 15:05:13 +0200

    update guide

--- lang/lang.fr.js
+++ lang/lang.fr.js
@@ -101,7 +101,7 @@ var lang = {
     //guide
     guide_introTitle:"Présentation générale",
     guide_intro0:"L'application est articulée autour de trois volets : le tableau, l'arbre et l'histoire clinique. " +
-    "La famille peut être chargée à partir d'un fichier existant ou créée et modifiée avec le tableau et l'interface graphique. " + "\n" +
+    "La famille peut être chargée à partir d'un fichier existant ou créée et modifiée avec le tableau et l'interface graphique. " +
     // guide_intro1:
     + "Les données sont sauvegardées en cache local et sont supprimées lorsque l'onglet du navigateur est fermé. " +
     "Par ailleurs, aucune donnée n'est envoyée au serveur ce qui garantie la confidentialité des données.",
